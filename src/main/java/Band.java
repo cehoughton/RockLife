9e413d73271ee60b55f1bc024ad3d03c86334dd7
chris <cehoughton71@gmail.com>
2016-03-02 10:22:29 -0800

    pass initial tests for band class

--- src/main/java/Band.java
+++ src/main/java/Band.java
@@ -14,4 +14,37 @@ public class Band {
   public int getId() {
     return mId;
   }
+
+  public String getName() {
+    return mName;
+  }
+
+  @Override
+  public boolean equals(Object otherBand){
+    if (!(otherBand instanceof Band)) {
+      return false;
+    } else {
+      Band newBand = (Band) otherBand;
+      return this.getName().equals(newBand.getName()) &&
+             this.getId() == newBand.getId();
+    }
+  }
+
+  public void save() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO bands (name) VALUES (:name)";
+      this.mId = (int) con.createQuery(sql, true)
+        .addParameter("name", this.mName)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  //READ
+  public static List<Band> all() {
+    String sql = "SELECT id AS mId, name AS mName FROM bands";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Band.class);
+    }
+  }
 }
